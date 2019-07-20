b8f621b95087a8aacb0a32d5f6b897e7ca08ff08
echizenyayota <virtualmisso30@gmail.com>
2019-07-20 15:43:18 +0900

    291 コードを変更したときに自動的にバンドルを実行したい [1]webpack-dev-serverをインストールする

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: `${__dirname}/dist`,
     filename: 'main.js'
   },
+  devServer: {
+    contentBase: './dist'
+  }
 };
